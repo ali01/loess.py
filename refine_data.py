df6fd0146a9ce44e6f9524c1192d7732e108dee0
Ali Yahya <alive@ali01.com>
2010-11-29 21:54:11 -0800

    data refined

--- refine_data.py
+++ refine_data.py
@@ -1,8 +1,10 @@
-with open('data/ebtrk_atlc.txt') as in_file:
+with open('data/raw_data.txt') as in_file:
   with open('data/data.txt', 'w') as out_file:
     for line in in_file:
       tokens = line.split()
       tokens = tokens[0:12]
+      tokens.pop(9)
       tokens.append('\n')
-      line = ' '.join(tokens)
-      out_file.write(line)
+      if not '-99' in tokens:
+        line = ' '.join(tokens)
+        out_file.write(line)
